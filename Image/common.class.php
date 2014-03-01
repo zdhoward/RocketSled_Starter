cab78e43467abf74f4e732065e10e8bf80b029eb
Zach Howard <zdhoward@icloud.com>
2014-03-01 00:43:32 -0500

    Title Example Added
    Added a Title example

--- Image/common.class.php
+++ Image/common.class.php
@@ -1,9 +1,18 @@
 <?php
 	class Common
 	{
+		// Testing
 		public function SayHello()
 		{
 			echo "Hello World!".PHP_EOL;
+		}
+		
+		// HTML
+		public function FormatHeader($title)
+		{
+			echo "<head>".PHP_EOL;
+			echo "<title>".$title."</title>".PHP_EOL;
+			echo "</head>".PHP_EOL;
 		}
 	}
 ?>
--- Image/display_images.class.php
+++ Image/display_images.class.php
@@ -5,6 +5,7 @@
 	{
 		public function run()
 		{
+			Common::FormatHeader("Welcome to 8 Bit Collective");
 			echo "Images:".PHP_EOL;
 			echo "<br /><img src = '../Data/Images/01.jpg'><br />".PHP_EOL;
 			Common::SayHello();
